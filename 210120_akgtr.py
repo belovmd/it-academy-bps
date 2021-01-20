c76eac1c1d75f0a31652e39d43c07ec2bca43840
AlexKaid <alex.kaidau@gmail.com>
2021-01-20 20:00:10 +0300

    Homework1: Alex Kaid

--- 210120_akgtr.py
+++ 210120_akgtr.py
@@ -5,4 +5,4 @@ print('Hi, %s.' % name)
 
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print("iteration {iteration} is {name}".format(iteration = i, name = name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
