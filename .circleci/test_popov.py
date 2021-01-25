d65eb3fccec6dc017225a6068fa76d78259213c0
popov-vitaliy <vitamax555@mail.ru>
2021-01-25 23:21:54 +0300

    test file added 1

--- .circleci/test_popov.py
+++ .circleci/test_popov.py
@@ -14,3 +14,9 @@ print(b/a)
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+#test4
+parents, babies = (1, 1)
+while babies < 100:
+    print ('This generation has {0} babies'.format(babies))
+    parents, babies = (babies, parents + babies)
