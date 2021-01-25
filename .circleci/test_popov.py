aa5e24f3fc812f6459ed1586a5d4f989269bf56c
popov-vitaliy <vitamax555@mail.ru>
2021-01-25 23:19:12 +0300

    test file added 1

--- .circleci/test_popov.py
+++ .circleci/test_popov.py
@@ -9,3 +9,8 @@ print(a**b)
 a=2
 b=8
 print(b/a)
+
+#test4
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
