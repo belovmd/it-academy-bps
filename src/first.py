cfed6aaacd1a1975e832881a949ba20d8fc0d414
Aliasta2088 <aliasta2088@gmail.com>
2021-01-25 20:43:14 +0300

    'message'

---
+++ src/first.py
@@ -0,0 +1,10 @@
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    import re
+
+    for test_string in ['555-1212', 'ILL-EGAL']:
+        if re.match(r'^\d{3}-\d{4}$', test_string):
+            print(test_string, 'is a valid US local phone number')
+        else:
+            print(test_string, 'rejected')
