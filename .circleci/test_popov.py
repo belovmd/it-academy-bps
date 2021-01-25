6da1415068072d121005a0883d415e3d2b80f64e
popov-vitaliy <vitamax555@mail.ru>
2021-01-25 23:26:35 +0300

    test file added 1

--- .circleci/test_popov.py
+++ .circleci/test_popov.py
@@ -20,3 +20,11 @@ parents, babies = (1, 1)
 while babies < 100:
     print ('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
+
+#test5
+def greet(name):
+    print ('Hello', name)
+
+greet('Jack')
+greet('Jill')
+greet('Bob')
