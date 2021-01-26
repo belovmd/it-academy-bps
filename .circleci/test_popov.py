9c60f84d763a0c1242f9257f70d5807baa1fc2c1
popov-vitaliy <vitamax555@mail.ru>
2021-01-26 12:05:29 +0300

    test file added 1

--- .circleci/test_popov.py
+++ .circleci/test_popov.py
@@ -28,3 +28,12 @@ def greet(name):
 greet('Jack')
 greet('Jill')
 greet('Bob')
+
+#test6
+import re
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print (test_string, 'is a valid US local phone number')
+    else:
+        print (test_string, 'rejected')
+
