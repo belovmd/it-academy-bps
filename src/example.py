44f8f881adde9033bedd13f23522759ce91ef082
Papelushka <zhanna.papalamova@gmail.com>
2021-01-26 20:36:13 +0300

    Homework1: Zhanna Papalamova_new

--- src/example.py
+++
@@ -1,7 +0,0 @@
-print('good night')
-name = input('What is your name?\n')
-print ('Hi, %s.' % name)
-friends = ['john', 'pat', 'gary', 'michael']
-for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
-
