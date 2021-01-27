c803819979bd8344a7b53332144315d37e4bf012
Anastasia Ashurko <nastyaashurko@gmail.com>
2021-01-27 20:12:47 +0300

    'massag7'

--- src/hw1.py
+++ src/hw1.py
@@ -21,15 +21,6 @@ greet('Jack')
 greet('Jill')
 greet('Bob')
 
-# This program adds up integers that have been passed as arguments in the command line
-from itertools import groupby
-import sys
-try:
-    total = sum(int(arg) for arg in sys.argv[1:])
-    print('sum =', total)
-except ValueError:
-    print('Please supply integer arguments')
-
 
     lines = '''
     This is the
@@ -57,3 +48,13 @@ except ValueError:
         print(REFRAIN % (bottles_of_beer, bottles_of_beer,
                          bottles_of_beer - 1))
         bottles_of_beer -= 1
+
+
+prices = {'apple': 0.40, 'banana': 0.50}
+my_purchase = {
+    'apple': 1,
+    'banana': 6}
+grocery_bill = sum(prices[fruit] * my_purchase[fruit]
+                   for fruit in my_purchase)
+print ('I owe the grocer $%.2f' % grocery_bill)
+
