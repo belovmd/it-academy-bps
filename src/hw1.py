029de8268eab09d811cb64d148054e44f36d75ce
Anastasia Ashurko <nastyaashurko@gmail.com>
2021-01-27 20:15:34 +0300

    'massage8'

--- src/hw1.py
+++ src/hw1.py
@@ -22,6 +22,7 @@ greet('Jill')
 greet('Bob')
 
 
+from itertools import groupby
     lines = '''
     This is the
     first paragraph.
@@ -56,5 +57,5 @@ my_purchase = {
     'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
-print ('I owe the grocer $%.2f' % grocery_bill)
+print('I owe the grocer $%.2f' % grocery_bill)
 
