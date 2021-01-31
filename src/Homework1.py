32e98e7ea01aa03c467c4575c331b78a79a07880
Aliasta2088 <aliasta2088@gmail.com>
2021-01-31 20:40:24 +0300

    Homework1:AstapekoS

--- src/Homework1.py
+++ src/Homework1.py
@@ -1,10 +1,3 @@
-import re
-for test_string in ['555-1212', 'ILL-EGAL']:
-    if re.match (r'^\d{3}-\d{4}$', test_string):
-        print(test_string, 'is a valid US local phone number')
-    else:
-        print(test_string, 'rejected')
-
 print('Hello, world!')
 name = input('What is your name?')
 print('His name')
@@ -16,11 +9,3 @@ while babies < 100:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
-
-def greet(name):
-    print('Hello', name)
-
-
-greet('Jack')
-greet('Jill')
-greet('Bob')
