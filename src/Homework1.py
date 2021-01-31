06fe0e74fd20ff02da75aac4b1254ab26dcf2170
Aliasta2088 <aliasta2088@gmail.com>
2021-01-31 20:30:05 +0300

    Homework1:AstapekoS

--- src/Homework1.py
+++ src/Homework1.py
@@ -1,6 +1,6 @@
 import re
 for test_string in ['555-1212', 'ILL-EGAL']:
-    if re.match(r'^\d{3}-\d{4}$', test_string):
+    if re.match (r'^\d{3}-\d{4}$', test_string):
         print(test_string, 'is a valid US local phone number')
     else:
         print(test_string, 'rejected')
@@ -19,6 +19,8 @@ while babies < 100:
 
 def greet(name):
     print('Hello', name)
+
+
 greet('Jack')
 greet('Jill')
 greet('Bob')
