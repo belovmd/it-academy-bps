6117387e752186ce3c47e43781fce2ebaeb629ca
Aliasta2088 <aliasta2088@gmail.com>
2021-01-31 20:42:40 +0300

    'Homework:1AstapenkoA2

--- src/Homework1.py
+++ src/Homework1.py
@@ -1,11 +1,3 @@
 print('Hello, world!')
 name = input('What is your name?')
 print('His name')
-friends: list[str] = ['john', 'pat', 'gary', 'michael']
-for i, name in enumerate(friends):
-    print("iteration {iteration} is {name}".format(iteration=i, name=name))
-parents, babies = (1, 1)
-while babies < 100:
-    print('This generation has {0} babies'.format(babies))
-    parents, babies = (babies, parents + babies)
-
