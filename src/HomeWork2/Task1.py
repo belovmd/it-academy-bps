15cff1022e4b568359640f98c46a555ace3d8de1
AlexKaid <alex.kaidau@gmail.com>
2021-02-04 23:10:05 +0300

    Homework2: Task1

--- src/HomeWork2/Task1.py
+++ src/HomeWork2/Task1.py
@@ -4,6 +4,17 @@
 Пример:
 Input: Цена одной вещи 3 рубля 20 копеек, посчитать 3 предмета.
 Output: Общая цена 9 рублей 60 копеек """
+import re
+
+str_input = input("Input:")
+str_nums = re.findall(r'\d+', str_input)
+str_nums = [int(i) for i in str_nums]
+if len(str_nums) >= 3:
+    amount = str_nums[0] * str_nums[2] + str_nums[1] / 100 * str_nums[2]
+    print('Amount to pay = {} Rub. {} Kop'.format(int(amount // 1), int(amount % 1 * 100)))
+else:
+    print('Incorrect Input')
+
 price_rub = int(input('Price Rub:'))
 price_kop = int(input('Price Kop:'))
 count_product = int(input('Count Product:'))
