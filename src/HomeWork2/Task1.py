1b89ade0284226865ddcda87d00ba1b60b8dd975
AlexKaid <alex.kaidau@gmail.com>
2021-02-04 22:44:23 +0300

    Homework2: Task1

--- src/HomeWork2/Task1.py
+++ src/HomeWork2/Task1.py
@@ -1,3 +1,12 @@
-# Task1
+""" 1. Напишите программу, которая считает общую цену. Вводится M рублей и N копеек цена, а также количество S товара
+Посчитайте общую цену в рублях и копейках за L товаров.
+Пример:
+Input: Цена одной вещи 3 рубля 20 копеек, посчитать 3 предмета.
+Output: Общая цена 9 рублей 60 копеек """
+price_rub = int(input('Price Rub:'))
+price_kop = int(input('Price Kop:'))
+count_product = int(input('Count Product:'))
+print('Amount to pay = {}'.format(price_rub * count_product + price_kop / 100 * count_product))
+amount = price_rub * count_product + price_kop / 100 * count_product
+print('Amount to pay = {} Rub. {} Kop'.format(int(amount // 1), int(amount % 1 * 100)))
 
-print("Its Task 1")
