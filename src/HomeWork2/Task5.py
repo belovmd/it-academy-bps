4e48019fb88506e14f192a0553a0cd0e8fb528ab
AlexKaid <alex.kaidau@gmail.com>
2021-02-07 17:40:56 +0300

    Homework2: Task 1,2,3,4,5,6,8

--- src/HomeWork2/Task5.py
+++ src/HomeWork2/Task5.py
@@ -1,16 +1,13 @@
-"""6. Определите, является ли число палиндромом
-(читается слева направо и справа налево одинаково).
-Число положительное целое, произвольной длины.
-Задача требует работать только с числами
-(без конвертации числа в строку или что-нибудь еще)"""
+"""5. Выведите n-ое число Фибоначчи, используя только временные переменные,
+циклические операторы и условные операторы. n - вводится"""
 
 
-def reverse_num(n, part=0):
-    if n == 0:
-        return part
-    return reverse_num(n // 10, part * 10 + n % 10)
+def fibonac(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
 
 
-number = 123454321
-print("Num a Palindrome!") if reverse_num(number) == number else print("Num not a Palindrome!")
-
+list_num = list(fibonac(10))
+print(list_num)
---
+++ src/HomeWork2/Task6.py
@@ -0,0 +1,15 @@
+"""6. Определите, является ли число палиндромом
+(читается слева направо и справа налево одинаково).
+Число положительное целое, произвольной длины.
+Задача требует работать только с числами
+(без конвертации числа в строку или что-нибудь еще)"""
+
+
+def reverse_num(n, part=0):
+    if n == 0:
+        return part
+    return reverse_num(n // 10, part * 10 + n % 10)
+
+
+number = 123454321
+print("Num a Palindrome!") if reverse_num(number) == number else print("Num not a Palindrome!")
