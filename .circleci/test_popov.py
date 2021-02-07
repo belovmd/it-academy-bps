547c2bf395d4074c210694aa2bd906d0c7ff2d01
popov-vitaliy <vitamax555@mail.ru>
2021-02-07 18:18:51 +0300

    test file added 1

--- .circleci/test_popov.py
+++ .circleci/test_popov.py
@@ -9,11 +9,11 @@ print('Hi, %s.' % name)
 # test 4
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
 # test 5
 def greet(name):
-    print ('Hello', name)
+    print('Hello', name)
 
 greet('Jack')
 greet('Jill')
@@ -22,7 +22,7 @@ greet('Bob')
 # test 6
 parents, babies = (1, 1)
 while babies < 100:
-    print ('This generation has {0} babies'.format(babies))
+    print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
 # test 7
@@ -32,7 +32,7 @@ my_purchase = {
     'banana': 6}
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
-print ('I owe the grocer $%.2f' % grocery_bill)
+print('I owe the grocer $%.2f' % grocery_bill)
 
 # test 8
 def max(a, b):
