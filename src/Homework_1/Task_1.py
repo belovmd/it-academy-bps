8ca5350ef44ac3eb010a0529551cc495fa3a1f9c
alexe <AlexeiKarnauhov@gmail.com>
2021-02-09 00:24:08 +0300

    Homework1: Alexei Karnauhov (Task_1)

--- src/Homework_1/Task_1.py
+++ src/Homework_1/Task_1.py
@@ -1,64 +1,80 @@
-"""1-я   задачка"""
+"""Зарегистрируйтесь на одном (или нескольких) из сайтов:
+https://py.checkio.org/ , https://www.codewars.com,
+https://www.hackerrank.com/,
+https://acmp.ru И решите 1-5 задач уровня Elementary и advanced.
+Поместите 3 простых и 2 сложных задачи на Ваш выбор в пул реквест."""
+
+"""Complete the function that accepts a string parameter, and reverses each word in the string.
+All spaces in the string should be retained."""
 
 
-def email_gen(list_of_names):
-    emails = []
-    for i in list_of_names:
-        letter = 1
-        while i[1] + '.' + i[0][0:letter] + '@company.io' in emails:
-            letter += 1
-        emails.append(i[1] + '.' + i[0][0:letter] + '@company.io')
-    return emails
+def reverse_words(text):
+    b = text.split(' ')
+    c = []
+    d = ' '
+    for i in b:
+        i = i[::-1]
+        c.append(i)
+    d = ' '.join(c)
+    return d
 
 
-strings = []
-f = open('D:\\AlexeiKarnauhov\\task_file.txt', 'r')
-for i in f:
-    strings.append(i.split(','))
-f.close()
-list_of_names = []
-n = 0
-for i in strings:
-    list_of_names.append([])
-    if i[0] == 'EMAIL' or len(i[3].replace(' ', '')) != 7 or ' \n' in i or len(i[1]) <= 1 or \
-            len(i[2]) <= 1 or i[3].replace(' ', '').isdigit() is False:
-        list_of_names[n].append('no_email')
-        list_of_names[n].append('no_email' + str(n))
-    else:
-        list_of_names[n].append(i[1].replace(' ', ''))
-        list_of_names[n].append(i[2].replace(' ', ''))
-    n += 1
-emails = (email_gen(list_of_names))
-print(strings)
-n = 0
-f = open('D:\\AlexeiKarnauhov\\task_file.txt', 'w')
-for i in strings:
-    if 'no_email' not in emails[n]:
-        i[0] = emails[n]
-    n += 1
-    f.write(i[0] + ',' + i[1] + ',' + i[2] + ',' + i[3] + ',' + i[4])
-f.close()
-
-"""и еще задачка - бинарный поиск"""
+"""Build Tower by the following given argument:
+number of floors (integer and always greater than 0)."""
 
 
-def binary_search(list, item):
-    low = 0
-    high = len(list) - 1
-
-    while low <= high:
-        mid = (low + high)
-        guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
+def tower_builder(n_floors):
+    floors = []
+    n = n_floors
+    for i in range(n_floors):
+        n -= 1
+        floors.append(' ' * n + '*' * (i * 2 + 1) + ' ' * n)
+    return floors
 
 
-my_list = [1, 3, 5, 7, 9]
+"""Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string,
+the longest possible,
+containing distinct letters - each taken only once - coming from s1 or s2."""
 
-print(binary_search(my_list, 3))
-print(binary_search(my_list, -1))
+
+def longest(s1, s2):
+    temp_set_1 = set(s1)
+    temp_set_2 = set(s2)
+    new_string_1 = ''.join(temp_set_1)
+    new_string_2 = ''.join(temp_set_2)
+    new_string_3 = new_string_1 + new_string_2
+    temp_set_3 = set(new_string_3)
+    new_4 = sorted(''.join(temp_set_3))
+    new_5 = ''.join(new_4)
+    return new_5
+
+
+"""Given two integers a and b, which can be positive or negative,
+find the sum of all the integers between including them too and return it.
+If the two numbers are equal return a or b."""
+
+
+def get_sum(a, b):
+    if b > a:
+        c = [int(i) for i in range(a, b + 1)]
+        get_sum = sum(c)
+        return get_sum
+    elif a > b:
+        c = [int(i) for i in range(b, a + 1)]
+        get_sum = sum(c)
+        return get_sum
+    elif a == b:
+        return a
+
+
+"""Write simple .camelCase method (camel_case function in PHP,
+CamelCase in C# or camelCase in Java) for strings.
+All words must have their first letter capitalized without spaces."""
+
+
+def camel_case(string):
+    import re
+    string_1 = string.title()
+    result = re.findall(r'\w+', string_1)
+    result_1 = ''.join(result)
+    return result_1
