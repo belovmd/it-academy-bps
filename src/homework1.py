47e9d99dfa79648dc948a1e45a50bf4a37b5daca
OlgaKosm <romanykolya@tut.by>
2021-02-27 12:10:16 +0300

    added describe of tasks

--- src/homework1.py
+++ src/homework1.py
@@ -9,19 +9,29 @@ from time import localtime
 import unittest
 import xml.etree.ElementTree as etree
 
+"""# 1 lines: Output"""
 print('Hello, world!')
 
+
+"""# 2 lines: Input"""
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
 
+
+"""# 3 lines: For loop, built-in enumerate function"""
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
+
+"""# 4 lines: Fibonacci, tuple assignment"""
 parents, babies = (1, 1)
 while babies < 10:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
+
+
+"""# 5 lines: Functions"""
 
 
 def greet(name):
@@ -32,12 +42,16 @@ greet('Jack')
 greet('Jill')
 greet('Bob')
 
+
+"""# 6 lines: Import, regular expressions"""
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
         print(test_string, 'is a valid US local phone number')
     else:
         print(test_string, 'rejected')
 
+
+"""# 7 lines: Dictionaries, generator expressions"""
 prices = {'apple': 0.40, 'banana': 0.50}
 my_purchase = {
     'apple': 1,
@@ -46,8 +60,9 @@ grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
 print('I owe the grocer $%.2f' % grocery_bill)
 
-# This program adds up integers that have been passed as arguments in the command line
 
+"""# 8 lines: Command line arguments, exception handling"""
+# This program adds up integers that have been passed as arguments in the command line
 
 try:
     total = sum(int(arg) for arg in sys.argv[1:])
@@ -55,10 +70,11 @@ try:
 except ValueError:
     print('Please supply integer arguments')
 
+
+"""# 9 lines:  Opening files"""
 # indent your Python code to put into an email
-
-
 # glob supports Unix style pathname extensions
+
 python_files = glob.glob('*.py')
 for file_name in sorted(python_files):
     print('    ------' + file_name)
@@ -69,6 +85,8 @@ for file_name in sorted(python_files):
 
     print()
 
+
+"""# 10 lines:  Time, conditionals, from..import, for..else"""
 activities = {8: 'Sleeping',
               9: 'Commuting',
               17: 'Working',
@@ -86,6 +104,8 @@ for activity_time in sorted(activities.keys()):
 else:
     print('Unknown, AFK or sleeping!')
 
+
+"""# 11 lines:  Triple-quoted strings, while loop"""
 REFRAIN = '''
 %d bottles of beer on the wall,
 %d bottles of beer,
@@ -97,6 +117,9 @@ while bottles_of_beer > 1:
     print(REFRAIN % (bottles_of_beer, bottles_of_beer,
                      bottles_of_beer - 1))
     bottles_of_beer -= 1
+
+
+"""# 12 lines:  Classes"""
 
 
 class BankAccount(object):
@@ -118,7 +141,35 @@ my_account.withdraw(50)
 print(my_account.balance, my_account.overdrawn())
 
 
+"""# 13 lines:  Unit testing with unittest"""
+
+
 def median(pool):
+    copy = sorted(pool)
+    size = len(copy)
+    if size % 2 == 1:
+        return copy[int((size - 1) / 2)]
+    else:
+        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
+
+
+class TestMedian(unittest.TestCase):
+    def testMedian(self):
+        self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+"""# 14 lines:  Doctest-based testing"""
+
+
+def median1(pool):
+    '''Statistical median to demonstrate doctest.
+        >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
+        6 #change to 7 in order to pass the test
+        '''
     copy = sorted(pool)
     size = len(copy)
     if size % 2 == 1:
@@ -129,9 +180,10 @@ def median(pool):
 
 if __name__ == '__main__':
     import doctest
-
     doctest.testmod()
 
+
+"""# 15 lines:  itertools"""
 lines = '''
 This is the
 first paragraph.
@@ -143,14 +195,15 @@ This is the second.
 for has_chars, frags in groupby(lines, bool):
     if has_chars:
         print(' '.join(frags))
-
-
 # PRINTS:
 # This is the first paragraph.
 # This is the second.
 
 
+"""# 16 lines:  csv module, tuple unpacking, cmp() built-in"""
 # need to define cmp function in Python 3
+
+
 def cmp(a, b):
     return (a > b) - (a < b)
 
@@ -173,6 +226,8 @@ with open('stocks.csv', 'r') as stocksFile:
         status = status_labels[cmp(float(change), 0.0)]
         print('%s is %s (%.2f)' % (name, status, float(pct)))
 
+
+"""# 18 lines:  8-Queens Problem (recursion)"""
 BOARD_SIZE = 8
 
 
@@ -203,6 +258,9 @@ for answer in solve(BOARD_SIZE):
     print(answer)
 
 
+"""# 20 lines:  Prime numbers sieve w/fancy generators"""
+
+
 def iter_primes():
     # an iterator of all numbers between 2 and +infinity
     numbers = itertools.count(2)
@@ -223,6 +281,8 @@ for p in iter_primes():
         break
     print(p)
 
+
+"""# 21 lines:  XML/HTML parsing"""
 dinner_recipe = '''<html><body><table>
 <tr><th>amt</th><th>unit</th><th>item</th></tr>
 <tr><td>24</td><td>slices</td><td>baguette</td></tr>
@@ -246,6 +306,8 @@ for ingredient in tree.getiterator('tr'):
     if item.tag == "td" and item.text not in pantry:
         print("%s: %s %s" % (item.text, amt.text, unit.text))
 
+
+"""# 28 lines:  8-Queens Problem (define your own exceptions)"""
 BOARD_SIZE = 8
 
 
@@ -279,6 +341,8 @@ queens = add_queen([])
 print(queens)
 print("\n".join(". " * q + "Q " + ". " * (BOARD_SIZE - q - 1) for q in queens))
 
+
+"""# 33 lines:  Guess the Number"""
 guesses_made = 0
 
 name = input('Hello! What is your name?\n')
@@ -305,21 +369,3 @@ if guess == number:
     print('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
 else:
     print('Nope. The number I was thinking of was {0}'.format(number))
-
-
-def median1(pool):
-    copy = sorted(pool)
-    size = len(copy)
-    if size % 2 == 1:
-        return copy[int((size - 1) / 2)]
-    else:
-        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
-
-
-class TestMedian(unittest.TestCase):
-    def testMedian(self):
-        self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
-
-
-if __name__ == '__main__':
-    unittest.main()
