82168920ed9c9d64d238a982c4d859cde8f16427
alexe <AlexeiKarnauhov@gmail.com>
2021-03-02 08:48:01 +0300

    Homework3: Alexei Karnauhov (task1) done

--- src/Homework_3/task1.py
+++ src/Homework_3/task1.py
@@ -3,3 +3,15 @@
 вместо чисел кратный 5 пишет Buzz, а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
 Задачу поместите в файл task1.py в папке src/homework3."""
 
+def FizzBuzz_4ange(n):
+
+    if n % 3 == 0 and n % 5 == 0:
+        return ("FizzBuzz")
+    elif n % 3 == 0:
+        return ("Fizz")
+    elif n % 5 == 0:
+        return ("Buzz")
+    else:
+        return str(n)
+
+print (' '.join(FizzBuzz_4ange(n) for n in range(1,101)))
