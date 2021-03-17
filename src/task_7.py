9f9db6beab4079b849288de70dc5f5c5ab5311e3
popov-vitaliy <vitamax555@mail.ru>
2021-03-17 11:21:19 +0300

    homework2: all tasks (edit 7)

--- src/task_7.py
+++ src/task_7.py
@@ -10,8 +10,8 @@ side_c = float(input("Введите длину стороны C: "))
 poluper = (side_a + side_b + side_c) / 2
 if side_a + side_b > side_c and side_a + side_c > side_b \
         and side_b + side_c > side_a:
-    square = sqrt(poluper * (poluper - side_a) * (poluper - side_b)
-                  * (poluper - side_c))
+    square = sqrt(poluper * (poluper - side_a) * (poluper - side_b) *
+                  (poluper - side_c))
     print("Площадь треугольника {}".format(square))
 else:
     print("Это не треугольник")
