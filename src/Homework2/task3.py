52a6b15eaf442915a544973d1194dfcbe5c13d8a
Papelushka <zhanna.papalamova@gmail.com>
2021-03-18 23:12:17 +0300

    task3_update

--- src/Homework2/task3.py
+++ src/Homework2/task3.py
@@ -1,9 +1,8 @@
 # 3. Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы.
 # Например, если было введено "abc cde def", то должно быть выведено "abcdef"
-s = input()
-str_ = s.replace(' ', '')
-s_new = ''
-for i in range(len(str_)):
-    if s_new.find(str_[i]) == -1:
-        s_new += str_[i]
-print(s_new)
+st = input()
+st_new = ''
+for i in st:
+    if i not in st_new and i != ' ':
+        st_new += i
+print(st_new)
