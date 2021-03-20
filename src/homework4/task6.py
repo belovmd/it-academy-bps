37b7e83183841cf125933645802a3191de8192b7
OlgaKosm <romanykolya@tut.by>
2021-03-20 20:53:13 +0300

    done task 1-7 /5

--- src/homework4/task6.py
+++ src/homework4/task6.py
@@ -1,16 +1,17 @@
 """# 6 task:  different word in text """
+import re
+import string
 
-my_string = "Hi, my little Tim. Sorry, Tim, for my last early call."
-
-
-
-
-my_string = "vjvj. dhпрпррпрпрпрdh, ghhg djdjd?jdjdjdj"
+my_string = "Hi, my little Tim. Sorry,    Tim, for my last early call."
 mas = re.split(' ', my_string)
-temp = ''
+diff_word = 0
 table = str.maketrans('', '', string.punctuation)
 stripped = [w.translate(table) for w in mas]
-for char in stripped:
-    if len(char) > len(temp):
-        temp = char
-print(temp)
+for i in stripped:
+    if i == '':
+        continue
+    if stripped.count(i) != 1:
+        continue
+    else:
+        diff_word += 1
+print(diff_word)
