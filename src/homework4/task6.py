5c513f49e29bf6b1b2657bfb4eaad1951acfec50
OlgaKosm <romanykolya@tut.by>
2021-03-20 20:08:21 +0300

    done task6

--- src/homework4/task6.py
+++ src/homework4/task6.py
@@ -1,2 +1,16 @@
 """# 6 task:  different word in text """
 
+my_string = "Hi, my little Tim. Sorry, Tim, for my last early call."
+
+
+
+
+my_string = "vjvj. dhпрпррпрпрпрdh, ghhg djdjd?jdjdjdj"
+mas = re.split(' ', my_string)
+temp = ''
+table = str.maketrans('', '', string.punctuation)
+stripped = [w.translate(table) for w in mas]
+for char in stripped:
+    if len(char) > len(temp):
+        temp = char
+print(temp)
