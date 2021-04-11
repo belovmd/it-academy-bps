6324cd36df1ab9a601c2ef117ed0909faf528f07
popov-vitaliy <vitamax555@mail.ru>
2021-04-11 12:35:20 +0300

    homework4: task2

--- src/task2.py
+++ src/task2.py
@@ -8,5 +8,21 @@
 выше. Выходные данные Для каждого из запроса выведите название страны,
 в котором находится данный город"""
 
-dct = {str(element + 1): (element + 1) ** 3 for element in range(20)}
-print(dct)
+n = int(input('Enter numbers of country ='))
+m = ()
+find_city = []
+country_cities = {}
+current_country_city = {}
+for i in range(0, n):
+    m = input('Enter country name and cities')
+    mass = m.split(' ')
+    current_country_city = {mass[0]: mass[1:len(mass)]}
+    country_cities.update(current_country_city)
+    city_number = int(input('Enter numbers of city ='))
+    for g in range(city_number):
+        city = input('Enter name of city')
+        find_city.append(city)
+    for key, values in country_cities.items():
+        for val in values:
+            if val in find_city:
+                print(key)
