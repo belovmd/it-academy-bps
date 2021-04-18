0de66e4bf6b48ae3afa3497f32210f3b52a5269b
AlexKaid <alex.kaidau@gmail.com>
2021-04-19 00:11:53 +0300

    homework_4: task 1,2,3,4,5,6,7

--- src/task5.py
+++ src/task5.py
@@ -1,2 +1,12 @@
 """Каждый из N школьников некоторой школы знает Mi языков. Определите,
 какие языки знают все школьники и языки, которые знает хотя бы один из школьников."""
+import random
+lst_lg = ('engl spani russian poland sweden turkey').split()
+n_pupi = 3
+dict = {i: [set(random.choices(lst_lg, k=random.randint(1, len(lst_lg))))] for i in range(0, n_pupi)}
+uniq_lg = set(lst_lg)
+for key in dict:
+    for value in dict[key]:
+        uniq_lg = set(uniq_lg) & set(value)
+        print(uniq_lg, set(value))
+print("Кол-во языков {} языки {}".format(len(uniq_lg), uniq_lg))
