3dd1460d1571100d864a4659e97a77e98469469c
AlexKaid <alex.kaidau@gmail.com>
2021-04-20 11:39:38 +0300

    homework_5: task 1,2,5

--- src/task5.py
+++ src/task5.py
@@ -3,6 +3,6 @@
 
 input_x = 10
 rez = 2
-while rez < input_x: #Не очень уверен в правильности , работает при x >= 2
+while rez < input_x:  # Не очень уверен в правильности , работает при x >= 2
     rez = rez << 2
 print("Наим.Ст2 от {} = {}".format(input_x, rez >> 2))
